5fa3f10714c83c830c1eecadb481d57585e670d9
Chris Figgins <figginsc@gmail.com>
2019-01-08 18:20:39 -0500

    Needed to update the length of the URL column

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE burgers_db.burgers(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`burger_name` VARCHAR(255) NOT NULL,
 	`description` VARCHAR(255) NOT NULL,
-	`image` VARCHAR(25) NOT NULL,
+	`image` VARCHAR(1024) NOT NULL,
 	`devoured` BOOLEAN DEFAULT false,
 	PRIMARY KEY(id)
 );
